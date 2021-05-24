f2c11c09f8b1c6920de2ea5d8f0f4dbad050d1bc
kingo233 <kingo233@outlook.com>
2021-05-25 06:22:32 +0800

    访问限流

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,6 +16,9 @@ class UsersController extends Controller
         $this->middleware('guest', [
             'only' => ['create']
         ]);
+        $this->middleware('throttle:10,60', [
+            'only' => ['store']
+        ]);
     }
     
     public function destroy(User $user)
